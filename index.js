743d8a4f6f0de64957ed7fa46d45a7214b2c02a9
John Corujo <corujojohn@gmail.com>
2017-11-18 16:49:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,11 +16,13 @@ function yourAnimal() {
 myAnimal();
 
 function add2(n) {
+  const two = 2
   return n + two
 
   // Feel free to move things around!
-  const two = 2
+  
 }
+add2()
 
 var funkyFunction = function() {
   return function() {
